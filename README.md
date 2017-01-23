809f3d9b0dce69a5a27c3d8cee516c0a8618dfd4
BearD01001 <d@vsv.io>
2017-01-23 10:32:24 +0800

    update README.md & demo files.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# paitcher
-A gentle page switcher for mulit page application / web site.
+<h1 align="center">Paitcher</h1>
+<h3 align="center">A gentle page switcher for mulit-page application / website.</h1>
+<p align="center">
+    <a href="https://github.com/BearD01001/paitcher/"><img src="https://img.shields.io/badge/version-1.0.0-green.svg" alt="version"></a>
+    <a href="https://github.com/BearD01001/paitcher/blob/master/LICENSE"><img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="license"></a>
+</p>
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var moment       = require('moment'),
     colors       = require('colors'),
     gulp         = require('gulp'),
     less         = require('gulp-less'),
+    gutil        = require('gulp-util'),
     babel        = require('gulp-babel'),
     watch        = require('gulp-watch'),
     rename       = require('gulp-rename'),
@@ -14,48 +15,48 @@ var moment       = require('moment'),
 var CONFIG       = require('./conf/gulp');
 
 var html2Sign    = () => {
-    return gulp.src(CONFIG.html.src)
-            .pipe(revAppend())
-            .pipe(gulp.dest(CONFIG.html.dist)) &&
-            console.log(`[${ moment().format('HH:mm:ss').gray }] ${ '*.html'.yellow } rebuilt successful.`);
+    gulp.src(CONFIG.html.src)
+        .pipe(revAppend())
+        .pipe(gulp.dest(CONFIG.html.dist)) &&
+        console.log(`[${ moment().format('HH:mm:ss').gray }] ${ '*.html'.blue } rebuilt successful.`);
 }
 
 gulp.task('html', () => {
-    return watch(CONFIG.html.src, (vinyl) => {
-                console.log(`[${ moment().format('HH:mm:ss').gray }] ${ vinyl.basename.yellow } rebuilt successful.`);
-            })
-            .pipe(revAppend())
-            .pipe(gulp.dest(CONFIG.html.dist));
+    watch(CONFIG.html.src, (vinyl) => {
+            console.log(`[${ moment().format('HH:mm:ss').gray }] ${ vinyl.basename.yellow } rebuilt successful.`);
+        })
+        .pipe(revAppend())
+        .pipe(gulp.dest(CONFIG.html.dist));
 });
 
 gulp.task('css', () => {
-    return watch(CONFIG.css.src, (vinyl) => {
-                console.log(`[${ moment().format('HH:mm:ss').gray }] ${ vinyl.basename.yellow } rebuilt successful.`);
-                html2Sign();
-            })
-            .pipe(plumber({ errHandler: e => CONFIG.plumberCatch(e) }))
-            .pipe(sourcemaps.init(CONFIG.sourcemaps.init))
-                .pipe(less(CONFIG.less))
-                .pipe(gulp.dest(CONFIG.css.dist))
-                .pipe(cleanCSS(CONFIG.cleanCSS))
-                .pipe(rename(CONFIG.rename))
-            .pipe(sourcemaps.write(CONFIG.sourcemaps.path, CONFIG.sourcemaps.write))
-            .pipe(gulp.dest(CONFIG.css.dist));
+    watch(CONFIG.css.src, (vinyl) => {
+            console.log(`[${ moment().format('HH:mm:ss').gray }] ${ vinyl.basename.red } rebuilt successful.`);
+            html2Sign();
+        })
+        .pipe(plumber({ errHandler: e => CONFIG.plumberCatch(e) }))
+        .pipe(sourcemaps.init(CONFIG.sourcemaps.init))
+            .pipe(less(CONFIG.less))
+            .pipe(gulp.dest(CONFIG.css.dist))
+            .pipe(cleanCSS(CONFIG.cleanCSS))
+            .pipe(rename(CONFIG.rename))
+        .pipe(sourcemaps.write(CONFIG.sourcemaps.path, CONFIG.sourcemaps.write))
+        .pipe(gulp.dest(CONFIG.css.dist));
 });
 
 gulp.task('js', () => {
-    return watch(CONFIG.js.src, (vinyl) => {
-                console.log(`[${ moment().format('HH:mm:ss').gray }] ${ vinyl.basename.yellow } rebuilt successful.`);
-                html2Sign();
-            })
-            .pipe(plumber({ errHandler: e => CONFIG.plumberCatch(e) }))
-            .pipe(sourcemaps.init(CONFIG.sourcemaps.init))
-                .pipe(babel())
-                .pipe(gulp.dest(CONFIG.js.dist))
-                .pipe(uglify(CONFIG.uglify))
-                .pipe(rename(CONFIG.rename))
-            .pipe(sourcemaps.write(CONFIG.sourcemaps.path, CONFIG.sourcemaps.write))
-            .pipe(gulp.dest(CONFIG.js.dist));
+    watch(CONFIG.js.src, (vinyl) => {
+            console.log(`[${ moment().format('HH:mm:ss').gray }] ${ vinyl.basename.yellow } rebuilt successful.`);
+            html2Sign();
+        })
+        .pipe(plumber({ errHandler: e => CONFIG.plumberCatch(e) }))
+        .pipe(sourcemaps.init(CONFIG.sourcemaps.init))
+            .pipe(babel())
+            .pipe(gulp.dest(CONFIG.js.dist))
+            .pipe(uglify(CONFIG.uglify))
+            .pipe(rename(CONFIG.rename))
+        .pipe(sourcemaps.write(CONFIG.sourcemaps.path, CONFIG.sourcemaps.write))
+        .pipe(gulp.dest(CONFIG.js.dist));
 });
 
 gulp.task('default', ['html', 'css', 'js']);
